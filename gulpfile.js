1269c58c13f6281581c0a4496676344a634c4bbf
Daniel Arteaga <dani8art.da@gmail.com>
2018-01-04 00:59:25 +0100

    [feat] added auto-deploy with serverless framework

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ const zip = require('gulp-zip');
 gulp.task('default', () => {
     let pkgName = process.env.npm_package_name;
     let version = process.env.npm_package_version;
-    gulp.src('src/*')
+    gulp.src('src/**')
         .pipe(zip(pkgName + '-' + version + '.zip'))
         .pipe(gulp.dest('dist'))
 });
