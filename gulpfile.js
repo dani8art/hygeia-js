8133160cd793944266bb176a755fe5265d925ba7
Daniel Arteaga <dani8art.da@gmail.com>
2018-01-29 21:13:56 +0100

    [feat] added license

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+'use strict';
+var gulp = require('gulp');
+var header = require('gulp-header');
+var pkg = require('./package.json');
+
+var banner = ['/**',
+    ' * <%= pkg.name %>', ,
+    ' * Copyright (c) 2018 darteaga (<%= pkg.homepage %>)',
+    ' * <%= pkg.license %> Licensed',
+    ' */',
+    '\n'].join('\n');
+
+gulp.task('default', () => {
+    gulp.src('./src/**')
+        .pipe(header(banner, { pkg: pkg }))
+        .pipe(gulp.dest('./src/'));
+});
+
